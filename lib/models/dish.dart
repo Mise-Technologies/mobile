d38bf1325b033da80a50be1de54a7e7d822aa07f
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-02-19 15:50:56 -0800

    Added allergents to Dish class

--- lib/models/dish.dart
+++ lib/models/dish.dart
@@ -1,6 +1,7 @@
 class Dish {
   final String name;
   final String description;
+  final List<String> allergies;
 
-  const Dish(this.name, this.description);
+  const Dish(this.name, this.description, this.allergies);
 }
